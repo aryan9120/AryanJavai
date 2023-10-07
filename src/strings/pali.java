1605e00ab520b5f99becc3a09eea74194962997c
Aryan Kumar Shaw Halwai <aryanshaw616@gmail.com>
2023-10-07 17:25:06 +0530

    strings_v1.0.1

--- src/strings/pali.java
+++ src/strings/pali.java
@@ -2,14 +2,18 @@ package strings;
 
 public class pali {
     public static void main(String[] args) {
-        String s="Aryan";String st=" ";char ch;
-        for(int i=s.length()-1;i>=0;i--)
-        {
-             ch=s.charAt(i);
-            st+=ch;
+        String s = "AmA";
+        String st = "";
+        char ch;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            ch = s.charAt(i);
+            st += ch;
         }
-        if(s.equals(st))
+        if (s.equals(st))
+        {
             System.out.println("Yes");
+        System.out.println("This is palindrome\n");
+    }
         else
             System.out.println("No");
     }
